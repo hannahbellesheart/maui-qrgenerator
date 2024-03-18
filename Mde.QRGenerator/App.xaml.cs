9d80547a5e31ea2551856c274d641ba720e21067
sigged <sderdeyn@gmail.com>
2024-03-18 11:56:41 +0100

    Add lifecycle events

--- Mde.QRGenerator/App.xaml.cs
+++ Mde.QRGenerator/App.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace Mde.QRGenerator
+﻿using System.Diagnostics;
+
+
+
+namespace Mde.QRGenerator
 {
     public partial class App : Application
     {
@@ -8,5 +12,43 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Created += Window_Created;
+            window.Activated += Window_Activated;
+            window.Deactivated += Window_Deactivated;
+            window.Resumed += Window_Resumed;
+            window.Destroying += Window_Destroying;
+
+            return window;
+        }
+
+        private void Window_Created(object sender, EventArgs e)
+        {
+            Debug.WriteLine("App meets world (CREATED)");
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            Debug.WriteLine("App is now active (ACTIVATED)");
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Debug.WriteLine("App is going on a holiday (DEACTIVATED)");
+        }
+
+        private void Window_Resumed(object sender, EventArgs e)
+        {
+            Debug.WriteLine("App is back from holiday (RESUMED)");
+        }
+
+        private void Window_Destroying(object sender, EventArgs e)
+        {
+            Debug.WriteLine("App is being destroyed (DESTROYING)");
+        }
+
     }
 }
